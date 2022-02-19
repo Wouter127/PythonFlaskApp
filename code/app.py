fb97f9d1a29e6892431a34a6b21ffcfec275d8e5
Wouter127 <r0785520@student.thomasmore.be>
2022-02-19 22:47:25 +0100

    section 5 done

--- code/app.py
+++ code/app.py
@@ -1,33 +1,10 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
-from werkzeug.security import safe_str_cmp
+from flask_restful import Api
+from flask_jwt import JWT
 
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-
-users = [
-    User(1, 'Wouter', 'Fem<3')
-]
-
-username_mapping = {user.username: user for user in users}
-userid_mapping = {user.id: user for user in users}
-
-
-def authenticate(username, password):
-    user = username_mapping.get(username, None)
-    if user and safe_str_cmp(user.password, password):
-        return user
-
-def identity(payload):
-    user_id = payload['identity']
-    return  userid_mapping.get(user_id, None)
-
+from security import authenticate, identity
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'wouter127'
@@ -35,51 +12,10 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price', type=float, required=True, help='this field cannot be left empty')
-
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-
-        data = Item.parser.parse_args()
-
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': "item deleted"}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
-
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)  # important to mention debug=True
--- code/security.py
+++ code/security.py
@@ -1,25 +1,12 @@
 from werkzeug.security import safe_str_cmp
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id
-        self.username = username
-        self.password = password
-
-
-users = [
-    User(1, 'Wouter', 'Fem<3')
-]
-
-username_mapping = {user.username: user for user in users}
-userid_mapping = {user.id: user for user in users}
+from user import User
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return  userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
