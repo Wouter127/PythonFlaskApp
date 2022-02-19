9836663eddb691b9f12d80f1e967016c41fe9b9f
Wouter127 <r0785520@student.thomasmore.be>
2022-02-19 14:18:34 +0100

    section 3 complete

--- app.py
+++ app.py
@@ -1,12 +1,74 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My wonderful store',
+        'items': [
+            {
+                'name': 'My item name',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+# POST /store/data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store nnot found'})
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+        return jsonify({'message': 'store nnot found'})
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store nnot found'})
+
+
+app.run(port=5000)
